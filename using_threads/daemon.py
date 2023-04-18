6db5f3106f0f43f4cd1fb49a45da47c530c8513a
onionmccabbage <onionmccabbage@live.com>
2023-04-18 12:20:34 +0100

    daemon sample

---
+++ using_threads/daemon.py
@@ -0,0 +1,19 @@
+# a daemon thread will continue to run until the main thread terminates
+from threading import Thread
+import time
+def standardThread():
+    print('starting standard thread')
+    time.sleep(5)
+    print('ending standard thread')
+
+def daemonThread():
+    while True: # an endless loop - use with caution
+        print('heartbeat.....')
+        time.sleep(0.25)
+
+if __name__ == '__main__':
+    s = Thread(target=standardThread)
+    d = Thread(target=daemonThread)
+    d.setDaemon(True) # now that thread will run continuously
+    s.start()
+    d.start()
