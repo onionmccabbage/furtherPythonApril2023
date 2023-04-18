a5cddbb4bae06d84e0c5880a445755f660d7a88f
onionmccabbage <onionmccabbage@live.com>
2023-04-18 16:26:18 +0100

    threadDecorator sample

--- using_threads/threadDecorator.py
+++ using_threads/threadDecorator.py
@@ -1,10 +1,55 @@
 # we can write decorators to apply to any function and make it thread-safe
+from threading import Lock
 
-def lock_a_method():
-    ''''''
+def lock_a_method(meth):
+    '''this can be used to decorate any methopd that needs to be thread safe (i.e. use locks)'''
+    if getattr(meth, '__is_locked', False):
+        raise TypeError(f'Method {meth} is already locked')
+    def locked_method(self, *args, **kwargs):
+        with self.__lock: # 'with' will release the lock when done
+            return meth(self, *args, **kwargs)
+    lock_a_method.__name__ = f'Locked Method {meth.__name__}'
+    locked_method.__is_locked = True
+    return locked_method
 
-def make_thread_safe():
-    ''''''
+def make_thread_safe(cls, method_names, lock_factory):
+    '''iterate over all the methods of a class making each thread safe'''
+    init = cls.__init__ # take a copy of the current __init__ for the class
+    def new_init(self, *args, **kwargs):
+        init(self, *args, **kwargs)
+        self.__lock = lock_factory
+    cls.__init__ = new_init # our decorated class now has a replacement __init__ method
+    # iterate over the methods of the class
+    for methodN in method_names:
+        old_meth = getattr(cls, methodN)
+        new_meth = lock_a_method(old_meth)
+        setattr(cls, methodN, new_meth) # replace the original method with the new version
+    return cls
 
+# here is a decorator to make selected class methods thread safe
+def lock_a_class(method_names, lock_factory):
+    return lambda cls: make_thread_safe(cls, method_names, lock_factory)
+
+# some arbitrary code
+@lock_a_class(['add', 'remove'], Lock)
+class MyClass(set): # here our class inherits from the 'set' class
+    def __init__(self, new_set): # since we extend the 'set' class, we need to provide a set
+        pass
+    @lock_a_method # here we apply a decorator to make this method thread-safe
+    def someMethod(self):
+        print(f'Here is an arbitrary method')
+    # def add(self):
+    #     print('This would be an add method')
+    # def remove(self):
+    #     print('This would be a remove method')
+        
 if __name__ == '__main__':
     ''''''
+    initial_set = {4,3,2}
+    s = MyClass(initial_set)
+    # s.add('apples')
+    # s.remove(4)
+    # see if 'someMethod' is locked
+    print(s.someMethod.__is_locked) # True
+    print(s.add.__is_locked) # True
+    print(s.remove.__is_locked) # True
