63d22a6113e39fc88b63dd284884d6e2980824b7
onionmccabbage <onionmccabbage@live.com>
2023-04-19 10:39:00 +0100

    process class

---
+++ using_processors/multiproc_class.py
@@ -0,0 +1,23 @@
+import multiprocessing
+import os
+import time
+
+class MyProcess(multiprocessing.Process):
+    def __init__(self):
+        super(MyProcess, self).__init__()
+    def run(self):
+        time.sleep(2)
+        print(f'Child process ID is {multiprocessing.current_process().pid}')
+
+def main():
+    procs = []
+    for n in range(os.cpu_count()): # NB you could also try GPU via CUDA
+        '''here we start new processes on their own process ID'''
+        p = MyProcess()
+        procs.append(p)
+        p.start()
+    for p in procs:
+        p.join()
+
+if __name__ == '__main__':
+    main()
