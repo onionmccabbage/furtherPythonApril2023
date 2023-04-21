fc02010b8f192d9d84ea59dcde592e32608e703a
onionmccabbage <onionmccabbage@live.com>
2023-04-21 10:45:56 +0100

    sample server

---
+++ microservices/my_server.py
@@ -0,0 +1,30 @@
+# microservices is a design pattern
+# break a system into parts containing specific solutions
+# each service can interact with other services
+# we often think of microservices as APIs
+
+import socket # this will provide a means for services to interact
+
+def myServer():
+    '''this microservice will receive requests from clients'''
+    # the following a typical default values for http interaction
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    param_t = ('localhost', 9876) # IP and port
+    server.bind(param_t)
+    # begin listening for requests
+    server.listen(6)
+    print(f'Server is listening on {param_t[0]}:{param_t[1]}')
+    #run the server continously
+    while True:
+        # begin accepting requests
+        (client, addr) = server.accept() # unpack the request into client and adress
+        buffer = client.recv(1024) # read the first 1024 bytes from the request
+        print(f'Server received {buffer}')
+        # in this server, we will simply echo back the sme buffer as CAPS
+        response_text = buffer.upper()
+        client.send(response_text)
+        if buffer == b'quit':
+            break
+
+if __name__ == '__main__':
+    myServer()
